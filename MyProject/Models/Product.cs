0191544b0f21b5b5b83815a9abd3b35d7e719785
shilpa-ss <shilpalankala10@gmail.com>
2020-09-29 10:29:29 +0530

    Added Product Model and Modified The Home->Index view

---
+++ MyProject/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public String ProductName { get; set; }
+
+        public int Quantity { get; set; }
+
+        public String Image { get; set; }
+    }
+}
